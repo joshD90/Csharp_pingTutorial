dd0f9a985f9e5329ecac43195f5e851a2436bbde
Joshua Dancey <joshuadancey@hotmail.com>
2023-05-14 12:00:57 +0100

    refactoring Ping service into object oriented programming, creating a pingService class

---
+++ Ping_Tutorial/PingService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace Ping_Tutorial
+{
+	public class PingService
+	{
+	
+		public string Data { get; set; }
+		public byte[] Buffer { get; set; }
+		public int Timeout { get; set; }
+		public string Address { get; set; }
+		public Ping pingSender { get; set; }
+		public PingOptions pingOptions { get; set; }
+		//this is our constructor
+		public PingService()
+		{
+			Timeout = 120;
+			Address = "4.2.2.2";
+			Data = "Learn to Code";
+			Buffer = Encoding.ASCII.GetBytes(Data);
+			pingSender = new Ping();
+			pingOptions = new PingOptions();
+			pingOptions.DontFragment = true;
+		}
+
+		public bool SendPing()
+		{
+            PingReply reply = pingSender.Send(Address, Timeout, Buffer, pingOptions);
+
+			if(reply.Status == IPStatus.Success)
+			{
+                Console.WriteLine("Success");
+                Console.WriteLine("Roundtrip: {0}", reply.RoundtripTime);
+                Console.WriteLine("Time to Live: {0}", reply.Options?.Ttl);
+                Console.WriteLine("Buffer: {0}:", reply.Buffer.Length);
+				return true;
+            }else
+			{
+				return false;
+			}
+        }
+	}
+}
+
--- Ping_Tutorial/Program.cs
+++ Ping_Tutorial/Program.cs
@@ -1,36 +1,12 @@
 ﻿using System.Net.NetworkInformation;
 using System.Text;
-//create our new ping object
-//this will ping Googles DNS server address 4.2.2.2
-Ping pingSender = new Ping();
+using Ping_Tutorial;
+//now we can use a class to carry out all this
+PingService pingService = new PingService();
+bool response = pingService.SendPing();
+Console.WriteLine(response);
 
 
-PingOptions pingOptions = new PingOptions();
-
-pingOptions.DontFragment = true;
-
-string data = "Learn to Code";
-byte[] buffer = Encoding.ASCII.GetBytes(data);
-int timeout = 120;
-string address = "4.2.2.2";
-
-PingReply reply = pingSender.Send(address,timeout,buffer,pingOptions);
-
-if(reply.Status == IPStatus.Success)
-{
-    Console.WriteLine("Success");
-
-    Console.WriteLine("Roundtrip: {0}", reply.RoundtripTime);
-    Console.WriteLine("Time to Live: {0}", reply.Options?.Ttl);
-    Console.WriteLine("Buffer: {0}:", reply.Buffer.Length);
-    
-}
-else
-{
-    Console.WriteLine("Unsuccessful");
-}
-
-Console.WriteLine("Response: {0}", reply?.Status.ToString());
 
 
 
